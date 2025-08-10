115c0dbd1656435ceebbc3538286f45265abe19d
PushEax <pusheax.vk@gmail.com>
2025-08-10 22:28:52 +0300

    add tailwind css v3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import NotActiveLabel from './components/not-active-label';
 export default function Home() {
   return (
     <main>
-      <h1>Home page</h1>
+      <h1 className="text-6xl">Home page</h1>
       <ActiveLabel>Active</ActiveLabel>
       <NotActiveLabel>Not active</NotActiveLabel>
     </main>
