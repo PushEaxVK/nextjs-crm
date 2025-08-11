52aa771c30e47d516d3189d1b518d7fe17b7412e
PushEax <pusheax.vk@gmail.com>
2025-08-12 02:38:09 +0300

    static and dynamic render test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import StatusLabel, { Status } from './components/stauts-label';
+import { headers } from 'next/headers';
+import StatusLabel, { Status } from '@/app/components/stauts-label';
+import Button from './components/button';
 
-export default function Home() {
+export default async function Home() {
+  const allHeaders = await headers();
+  console.log(Object.fromEntries(allHeaders));
   return (
     <main>
-      <h1 className="text-xl">Home page</h1>
+      <h1 className="text-xl">Home page {new Date().toTimeString()}</h1>
+      <Button />
       <StatusLabel status={Status.Active}>Active</StatusLabel>
       <StatusLabel status={Status.NotActive}>Not active</StatusLabel>
       <StatusLabel status={Status.Pending}>Pending</StatusLabel>
