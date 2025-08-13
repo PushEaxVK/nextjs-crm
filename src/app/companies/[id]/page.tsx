a37ee484578be8b4e34cd7ad61bdf83b4821292b
PushEax <pusheax.vk@gmail.com>
2025-08-14 00:18:59 +0300

    static render for params

--- src/app/companies/[id]/page.tsx
+++ src/app/companies/[id]/page.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import Header from '@/app/components/header';
 
-export interface PageProps {}
+export interface PageProps {
+  params: Promise<{ id: string }>; // Assuming the ID is passed as a route paramet
+}
 
-export default function Page({}: PageProps) {
+export function generateStaticParams() {
+  return [
+    { id: '1' }, // Example data, replace with actual data or logic to generate static params dynamically
+    { id: '2' },
+    { id: '3' },
+  ]; // Add more IDs as needed
+}
+
+export default async function Page({ params }: PageProps) {
+  const { id } = await params;
   return (
     <>
-      <Header>Company</Header>
+      <Header>Company ({id})</Header>
+      <p>{new Date().toTimeString()}</p>
     </>
   );
 }
