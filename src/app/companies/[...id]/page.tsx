04ccb411a8bd4d46019f3d91cc4863a4207b4a98
PushEax <pusheax.vk@gmail.com>
2025-08-14 00:21:23 +0300

    dynamic params

--- src/app/companies/[...id]/page.tsx
+++ src/app/companies/[...id]/page.tsx
@@ -5,19 +5,11 @@ export interface PageProps {
   params: Promise<{ id: string }>; // Assuming the ID is passed as a route paramet
 }
 
-export function generateStaticParams() {
-  return [
-    { id: '1' }, // Example data, replace with actual data or logic to generate static params dynamically
-    { id: '2' },
-    { id: '3' },
-  ]; // Add more IDs as needed
-}
-
 export default async function Page({ params }: PageProps) {
   const { id } = await params;
   return (
     <>
-      <Header>Company ({id})</Header>
+      <Header>Company ({String(id)})</Header>
       <p>{new Date().toTimeString()}</p>
     </>
   );
