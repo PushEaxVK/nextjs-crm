ccd1cd54945ea8332d9d041cc8ef7abb6c62dd5a
PushEax <pusheax.vk@gmail.com>
2025-08-13 23:55:22 +0300

    change companies page

--- src/app/companies/page.tsx
+++ src/app/companies/page.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import Header from '@/app/components/header';
+import Toolbar from '@/app/components/toolbar';
+import SearchInput from '@/app/components/search-input';
+import AddCompanyButton from '@/app/components/add-company-button';
+import CompanyTable from '@/app/components/company-table';
+import CompanyRow from '@/app/components/company-row';
+import { Status } from '@/app/components/stauts-label';
 
 export interface PageProps {}
 
 export default function Page({}: PageProps) {
   return (
-    <main>
-      <h1 className="text-xl">Companies Page</h1>
-    </main>
+    <>
+      <Header>Compaies</Header>
+      <Toolbar action={<AddCompanyButton />}>
+        <SearchInput />
+      </Toolbar>
+      <CompanyTable>
+        <CompanyRow
+          id={1}
+          category={'Product'}
+          company={'Costco'}
+          status={Status.Pending}
+          promotion={true}
+          county={'USA'}
+          joinedDate={'02.19.2023'}
+        />
+      </CompanyTable>
+    </>
   );
 }
