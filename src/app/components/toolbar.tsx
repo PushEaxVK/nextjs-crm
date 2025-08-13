9fec5b964b89022fd2d811dd3c69da32e58a0dc7
PushEax <pusheax.vk@gmail.com>
2025-08-13 23:54:55 +0300

    fix toolbar props

--- src/app/components/toolbar.tsx
+++ src/app/components/toolbar.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 export interface ToolbarProps {
-  chidren: React.ReactNode;
+  children: React.ReactNode;
   action?: React.ReactNode;
 }
 
-export default function Toolbar({ chidren, action }: ToolbarProps) {
+export default function Toolbar({ children, action }: ToolbarProps) {
   return (
     <div className="flex items-center gap-7 py-8 px-10">
-      <div className="flex-1">{chidren}</div>
+      <div className="flex-1">{children}</div>
       {action}
     </div>
   );
