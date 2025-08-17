3929e02dfa1e7a124c61e75e23b4c361200bb10d
PushEax <pusheax.vk@gmail.com>
2025-08-17 18:30:34 +0300

    add not found error for companies

---
+++ src/app/(admin)/companies/[...id]/not-found.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+import React from 'react';
+
+//eslint-disable-next-line @typescript-eslint/no-empty-object-type
+export interface NotFoundProps {}
+
+export default function NotFound({}: NotFoundProps) {
+  return (
+    <div>
+      <p>Could not found company</p>
+      <Link href="/companies" className="text-blue-500">
+        Back to companies
+      </Link>
+    </div>
+  );
+}
