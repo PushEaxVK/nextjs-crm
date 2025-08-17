2d93f02ca9372b80a578f07a63dc73fa6de0a2bd
PushEax <pusheax.vk@gmail.com>
2025-08-17 23:30:04 +0300

    add categories to dashboard

--- src/app/(admin)/dashboard/@categories/page.tsx
+++ src/app/(admin)/dashboard/@categories/page.tsx
@@ -1,3 +1,5 @@
+import DashboardCard from '@/app/components/dashboard-card';
+import StatCard, { StatCardType } from '@/app/components/stat-card';
 import { getSummaryCategories } from '@/lib/api';
 import React from 'react';
 
@@ -9,5 +11,19 @@ export interface PageProps {
 export default async function Page({}: PageProps) {
   const data = await getSummaryCategories();
 
-  return <div></div>;
+  return (
+    <DashboardCard label="Categories of companies">
+      <div className="grid grid-cols-12 gap-3 pb-5 px-5">
+        {data.map(({ categoryId, categoryTitle, count }) => (
+          <div key={categoryId} className="col-span-3">
+            <StatCard
+              type={StatCardType.Dark}
+              label={categoryTitle}
+              counter={count}
+            />
+          </div>
+        ))}
+      </div>
+    </DashboardCard>
+  );
 }
