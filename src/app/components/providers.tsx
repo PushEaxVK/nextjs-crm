3618c3a8d5b6304496567e7f3c0a35a53cc6e4df
PushEax <pusheax.vk@gmail.com>
2025-10-05 11:37:25 +0300

    update query provider

--- src/app/components/providers.tsx
+++ src/app/components/providers.tsx
@@ -1,13 +1,10 @@
+'use client';
+
+import React, { useMemo } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import React, { useMemo } from 'react';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface ProvidersProps extends React.PropsWithChildren {
-  //;
-}
-
-export default function Providers({ children }: ProvidersProps) {
+export default function Providers({ children }: React.PropsWithChildren) {
   const client = useMemo(() => new QueryClient(), []);
   return (
     <QueryClientProvider client={client}>
